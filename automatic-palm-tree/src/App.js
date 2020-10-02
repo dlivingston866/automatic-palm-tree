2eeb2886546eef27272e161e339a112e3e1db390
Doug Livingston <dliving1.dl@gmail.com>
2020-10-02 15:21:49 -0700

    added table html

--- automatic-palm-tree/src/App.js
+++ automatic-palm-tree/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+function App (){
+  const [employees, setEmployees] = useState([]);
+  
+  useEffect(() => {
+    if (employees === null || employees.length === 0){
+      fetch(`https://randomuser.me/api/?results=20&nat=us&inc=name,email,phone,id,dob,picture`)
+      .then(res => res.json())
+      .then(json => setEmployees(json.results )); 
+    }
+   
+      
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <table 
+    id="table"
+    data-toggle="table"
+    class="table table-hover table-striped">
+     <thead class="thead-dark">
+    <tr>
+    <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email Address</th>
+      <th scope="col">Phone Number</th>
+      <th scope="col">Age</th>
+    </tr>
+  </thead>
+        {employees.map(el => (
+          <tr>
+           <td>{el.name.first}</td>
+            <td>{el.name.last}</td>
+            <td>{el.email}</td>
+             <td>{el.phone}</td>
+              <td>{el.dob.age}</td>
+          </tr>
+        ))}
+      
+    </table>
   );
 }
 
 export default App;
+
